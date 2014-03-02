f4235c68c33a1a32ee077585c2db34eb695c3311
Atsuya Takagi <asoftonight@gmail.com>
2014-03-02 21:48:42 +0900

    add quicksort

---
+++ quicksort/QuickSort.java
@@ -0,0 +1,55 @@
+import java.util.Vector;
+
+public class QuickSort {
+  public QuickSort() {
+  }
+
+  public Vector<Integer> sort(Vector<Integer> list) {
+    if (list.size() <= 1) {
+      return list;
+    }
+
+    Integer pivot = list.remove(list.size() - 1);
+    Vector<Integer> less = new Vector<Integer>();
+    Vector<Integer> greater = new Vector<Integer>();
+    for (Integer value : list) {
+      if (value.intValue() <= pivot.intValue()) {
+        less.add(value);
+      } else {
+        greater.add(value);
+      }
+    }
+
+    Vector<Integer> result = new Vector<Integer>();
+    result.addAll(this.sort(less));
+    result.add(pivot);
+    result.addAll(this.sort(greater));
+
+    return result;
+  }
+
+  public void print(Vector<Integer> list) {
+    for (Integer value : list) {
+      System.out.printf("%d ", value.intValue());
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    Vector<Integer> list = new Vector<Integer>();
+    list.add(new Integer(9));
+    list.add(new Integer(8));
+    list.add(new Integer(7));
+    list.add(new Integer(6));
+    list.add(new Integer(5));
+    list.add(new Integer(4));
+    list.add(new Integer(3));
+    list.add(new Integer(2));
+    list.add(new Integer(1));
+
+    QuickSort quickSort = new QuickSort();
+    quickSort.print(list);
+    Vector<Integer> result = quickSort.sort(list);
+    quickSort.print(result);
+  }
+}
